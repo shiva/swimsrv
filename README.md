d37f0ad8ec83c4929e05f5df14aab26d205e061c
Shiva <shiv@shiv.me>
2014-04-06 20:50:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+swim server
+
+Sample RESTful server written using golang and code.google.com/p/gorest
+
+At the moment, it serves a static JSON file
---
+++ swimsrv.go
@@ -0,0 +1,26 @@
+package main
+import (
+        "code.google.com/p/gorest"
+        "net/http"
+       )
+
+func main() {
+    gorest.RegisterService(new(HelloService)) //Register our service
+        http.Handle("/",gorest.Handle())    
+        http.ListenAndServe(":8000",nil)
+}
+
+//Service Definition
+type HelloService struct {
+    gorest.RestService `root:"/tutorial/"`
+        helloWorld  gorest.EndPoint `method:"GET" path:"/hello-world/" output:"string"`
+        sayHello    gorest.EndPoint `method:"GET" path:"/hello/{name:string}" output:"string"`
+}
+
+func(serv HelloService) HelloWorld() string{
+    return "Hello World"
+}
+
+func(serv HelloService) SayHello(name string) string{
+    return "Hello " + name
+}
