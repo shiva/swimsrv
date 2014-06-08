9dba5b17dc9421462740da5c4479fdeb37e587d7
shiva <shiv@shiv.me>
2014-06-07 18:13:00 -0700

    REST server

--- swimsrv.go
+++ swimsrv.go
@@ -1,26 +1,36 @@
 package main
+
 import (
-        "code.google.com/p/gorest"
-        "net/http"
-       )
+    "code.google.com/p/gorest"
+    "net/http"
+)
 
 func main() {
     gorest.RegisterService(new(HelloService)) //Register our service
-        http.Handle("/",gorest.Handle())    
-        http.ListenAndServe(":8000",nil)
+    http.Handle("/", gorest.Handle())
+    http.ListenAndServe(":8000", nil)
 }
 
 //Service Definition
 type HelloService struct {
     gorest.RestService `root:"/tutorial/"`
-        helloWorld  gorest.EndPoint `method:"GET" path:"/hello-world/" output:"string"`
-        sayHello    gorest.EndPoint `method:"GET" path:"/hello/{name:string}" output:"string"`
+    helloWorld         gorest.EndPoint `method:"GET" path:"/hello-world/" output:"string"`
+    sayHello           gorest.EndPoint `method:"GET" path:"/hello/{name:string}" output:"string"`
+    getJSON            gorest.EndPoint `method:"GET" path:"/get-json/" output:"ItemStore"
 }
 
-func(serv HelloService) HelloWorld() string{
+func (serv HelloService) HelloWorld() string {
     return "Hello World"
 }
 
-func(serv HelloService) SayHello(name string) string{
+func (serv HelloService) SayHello(name string) string {
     return "Hello " + name
+}
+
+type ItemStore struct {
+    Items []Item
+}
+
+func (serv HelloService) GetJSON() ItemStore {
+    return
 }
